cf980bf47596b71c343ea815933391218642c48f
Guru18650 <guruwszystkiego@gmail.com>
2023-10-06 15:44:33 +0200

    lol

---
+++ ml_waluty/currency_handler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace ml_waluty
+{
+    public static class currency_handler
+    {
+        private static readonly HttpClient client = new HttpClient();
+        private static readonly string bankUrl = "https://api.nbp.pl/api/exchangerates/tables/C/";
+
+        public static async Task<List<currencyClass>> getCurrencies(){
+            var response = await client.GetStringAsync(bankUrl);
+            var r = JsonConvert.DeserializeObject<List<currencyClass>>(response);
+            return r;
+        }
+    }
+
+    public class currencyClass
+    {
+        string currency { get; set; }
+        string code { get; set; }
+        double bid { get; set; }
+        double ask { get; set; }
+    }
+}
